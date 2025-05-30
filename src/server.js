161b23ac37d8cc420e8d60769fb8f46a21a70099
truongntn <truongntn@gmail.com>
2025-05-30 09:33:05 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -96,6 +96,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', async (req, res) =>  {
-  await getBookingPressAppointments();
+  //await getBookingPressAppointments();
   res.send('...')
 })
